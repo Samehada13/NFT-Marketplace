289810f00ea90dc694ae81097d6eee45912fc835
Samehada13 <151368348+Samehada13@users.noreply.github.com>
2025-10-13 18:21:48 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   reactStrictMode: true,
    images: {
     // domains: ["gateway.pinata.cloud"],
-     domains: ["coral-elderly-takin-229.mypinata.cloud", "gateway.pinata.cloud"],
+     domains: ["coral-elderly-takin-229.mypinata.cloud"],
     formats: ["image/webp"],
   }
 };
